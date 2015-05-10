6d14743834540c2d616cfaae7f75f6f0fe05c01f
Juliusername <juliusodb@gmail.com>
2015-05-10 22:42:06 +0200

    Opdracht 2 - Auction

---
+++ SE42 auction/auction/target/generated-sources/annotations/auction/domain/User_.java
@@ -0,0 +1,14 @@
+package auction.domain;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-05-10T22:40:05")
+@StaticMetamodel(User.class)
+public class User_ { 
+
+    public static volatile SingularAttribute<User, Long> Id;
+    public static volatile SingularAttribute<User, String> email;
+
+}
